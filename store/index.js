4ec7c3b4108561c410d31499691a227470b4f3dd
Adrian Payne <adrian.payne@aida.de>
2017-11-06 15:53:04 +0100

    feat(stream): adds channel data from twitch api to store

--- store/index.js
+++ store/index.js
@@ -4,22 +4,38 @@ import axios from 'axios'
 const CLIENT_ID = '2w88chpjfzvxhtm2nsjf9oz2i6iz99'
 const API_URL = 'https://api.twitch.tv/kraken'
 const CHANNEL_NAME = 'jimothyz'
-const ENDPOINT_STREAMS = 'streams'
+const ENDPOINT_STREAM = 'streams'
+const ENDPOINT_CHANNEL = 'channels'
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      streamStatus: null
+      streamStatus: null,
+      channelData: {
+        status: '',
+        game: '',
+        partner: false,
+        views: 0,
+        followers: 0
+      }
     },
     mutations: {
-      SET_STEAMSTATUS (state, data) {
+      SET_STEAM_STATUS (state, data) {
         state.streamStatus = data
+      },
+
+      SET_CHANNEL_DATA (state, data) {
+        state.channelData = data
       }
     },
     actions: {
       async nuxtServerInit ({commit}, {req}) {
-        let { data } = await axios.get(`${API_URL}/${ENDPOINT_STREAMS}/${CHANNEL_NAME}?client_id=${CLIENT_ID}`)
-        commit('SET_STEAMSTATUS', data.stream)
+        const streamsResponse = await axios.get(`${API_URL}/${ENDPOINT_STREAM}/${CHANNEL_NAME}?client_id=${CLIENT_ID}`)
+        commit('SET_STEAM_STATUS', streamsResponse.data.stream)
+
+        const channelsResponse = await axios.get(`${API_URL}/${ENDPOINT_CHANNEL}/${CHANNEL_NAME}?client_id=${CLIENT_ID}`)
+        const { status, game, partner, views, followers } = channelsResponse.data
+        commit('SET_CHANNEL_DATA', { status, game, partner, views, followers })
       }
     }
   })
