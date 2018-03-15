203d4d09b401fd51de6dbbc7cc1de718cf0df48c
dazulu <definitive@outlook.com>
2018-03-15 15:39:32 +0100

    feat(schedule): add temp schedule page

--- components/organisms/Schedule.vue
+++ components/organisms/Schedule.vue
@@ -2,18 +2,20 @@
   <div class="contain">
     <h1 id="schedule" class="section__title">Stream Schedule</h1>
     <div class="schedule-wrapper">
-      boop
+      <img src="~/static/images/temp-schedule.jpg" class="schedule"/>
     </div>
   </div>
 </template>
-
-<script>
-</script>
 
 <style lang="scss" scoped>
   .schedule-wrapper {
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+
+  .schedule {
+    max-width: 1024px;
+    width: 100%;
   }
 </style>
