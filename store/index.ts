8cb5ebd460d4a19acddc98adf802660155b26e51
dazulu <definitive@outlook.com>
2021-03-13 22:41:26 +0100

    feat(store): add videos

--- store/index.ts
+++ store/index.ts
@@ -13,17 +13,33 @@ export interface Stream {
   boxArt?: string
 }
 
+export interface Video {
+  videoId: string
+  title: string
+  image: {
+    url: string
+    width: number
+    height: number
+  }
+}
+
+export type Videos = Array<Video>
+
 export const state = () => ({
   stream: {} as Stream,
+  videos: {} as Videos,
 })
 
 export type RootState = ReturnType<typeof state>
 
 export const getters: GetterTree<RootState, RootState> = {
   stream: (state) => state.stream,
+  videos: (state) => state.videos,
 }
 
 export const mutations: MutationTree<RootState> = {
   SET_STREAM_DATA: (state, data: Stream) =>
     (state.stream = { ...state.stream, ...data }),
+  SET_VIDEOS_DATA: (state, data: Videos) =>
+    (state.videos = { ...state.videos, ...data }),
 }
